f669d04a4ec00935fce2ff90b24d04f238c9e8f4
MXC1 <mfhcor@gmail.com>
2025-03-05 21:50:15 +0000

    Cleaner test format

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,18 @@
-describe('GIVEN I visit the app', () => {
-  describe('WHEN I click the login button', () => {
-    it('THEN sets auth url when login button is clicked', () => {
-      cy.visit('http://localhost:3000/');
+describe('GIVEN I visit the app \
+  WHEN I click the login button', () => {
+  it('THEN it sets auth url', () => {
+    cy.visit('http://localhost:3000/');
 
-      cy.window().should('have.property', 'redirectToSpotifyAuth')
+    cy.window().should('have.property', 'redirectToSpotifyAuth')
 
-      cy.window().then(win => {
-        cy.stub(win, 'redirectToSpotifyAuth')
-          .as('redirect');
-      })
-
-      cy.get('.login-button').click()
-
-      cy.get('@redirect').should('be.called')
+    cy.window().then(win => {
+      cy.stub(win, 'redirectToSpotifyAuth')
+        .as('redirect');
     })
+
+    cy.get('.login-button').click()
+
+    cy.get('@redirect').should('be.called')
   })
 });
 
