24b34e130f9f15fbb947ac5f7dae6bb8ddff7916
MXC1 <mfhcor@gmail.com>
2025-03-05 21:19:40 +0000

    Formatting

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,18 +2,18 @@ describe('GIVEN I visit the app', () => {
   describe('WHEN I click the login button', () => {
     it('THEN sets auth url when login button is clicked', () => {
       cy.visit('http://localhost:3000/');
-      
+
       cy.window().should('have.property', 'redirectToSpotifyAuth')
-      
-    cy.window().then(win => {
-      cy.stub(win, 'redirectToSpotifyAuth')
-        .as('redirect');
+
+      cy.window().then(win => {
+        cy.stub(win, 'redirectToSpotifyAuth')
+          .as('redirect');
+      })
+
+      cy.get('.login-button').click()
+
+      cy.get('@redirect').should('be.called')
     })
-
-    cy.get('.login-button').click()
-
-    cy.get('@redirect').should('be.called')
-  })
   })
 });
 
