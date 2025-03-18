a1eb38e3deae6dd5fabfffff3d4313ec2bd9115f
MXC1 <mfhcor@gmail.com>
2025-03-18 16:30:41 +0000

    Amend and refactor privacyPolicy test

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,14 +1,23 @@
 const authenticateAndVisitPrivacyPolicy = () => {
-    cy.setIndexedDbData("auth", "spotify_code_verifier", "valid_code_verifier");
-    cy.visit('/genre-album-map?code=valid_token&state=valid_state');
+    authenticate();
     cy.get('.menu-button').click();
     cy.get('.menu-item-button').contains('Privacy Policy').click();
+};
+
+const authenticate = () => {
+    cy.setIndexedDbData("auth", "spotify_code_verifier", "valid_code_verifier");
+    cy.visit('/genre-album-map?code=valid_token&state=valid_state');
 };
 
 const clickHomeLink = () => {
     cy.get('.menu-button').click();
     cy.get('.menu-item-button').contains('Home').click();
 };
+
+const clickPrivacyPolicyLink = () => {
+    cy.get('.menu-button').click();
+    cy.get('.menu-item-button').contains('Privacy Policy').click();
+};  
 
 beforeEach(() => {
     cy.intercept('GET', 'https://api.spotify.com/v1/me/albums*', { fixture: "mockGetMySavedAlbumsResponse.json" });
@@ -18,22 +27,30 @@ beforeEach(() => {
 });
 
 describe('GIVEN I have authenticated', () => {
-    beforeEach(authenticateAndVisitPrivacyPolicy);
+    beforeEach(() => {
+        authenticate();
+    });
 
-    describe('WHEN I open the hamburger menu', () => {
-        describe('WHEN I click the privacy policy link', () => {
-            it('THEN the header should be updated', () => {
-                cy.get('.page-title').should('contain', 'Privacy policy');
-                cy.get('.refresh-button').should('exist');
-            });
+    describe('WHEN I open the hamburger menu and click the privacy policy link', () => {
+        beforeEach(() => {
+            clickPrivacyPolicyLink();
+        });
 
-            it('THEN the privacy policy should be displayed', () => {
-                cy.get('.privacy-policy-container').should('exist');
-            });
+        it('THEN the header should be updated', () => {
+            cy.get('.page-title').should('contain', 'Privacy policy');
+            cy.get('.home-button').should('exist');
+        });
+
+        it('THEN the privacy policy should be displayed', () => {
+            cy.get('.privacy-policy-container').should('exist');
         });
     });
 
     describe('AND I am on the privacy policy page', () => {
+        beforeEach(() => {
+            authenticateAndVisitPrivacyPolicy();
+        });
+
         describe('WHEN I open the hamburger menu AND I click the home link', () => {
             it('THEN the genre grid should be shown', () => {
                 clickHomeLink();
@@ -43,12 +60,11 @@ describe('GIVEN I have authenticated', () => {
 
         describe('WHEN I click the home button', () => {
             it('THEN the genre grid should be shown', () => {
-                cy.get('.refresh-button').click();
+                cy.get('.home-button').click();
                 cy.get('.genre-grid').should('exist');
             });
 
             it('THEN the header should be updated', () => {
-                cy.get('.refresh-button').click();
                 cy.get('.page-title').should('contain', 'Your album library');
                 cy.get('.refresh-button').should('exist');
             });
@@ -57,11 +73,10 @@ describe('GIVEN I have authenticated', () => {
 });
 
 describe('GIVEN I have not authenticated', () => {
-    beforeEach(() => {
-        cy.visit('/privacy-policy');
-    });
-
     describe('AND I am on the privacy policy page', () => {
+        beforeEach(() => {
+            cy.visit('/privacy-policy');
+        });
         describe('WHEN I click the hamburger menu home link', () => {
             it('THEN the login page should be shown', () => {
                 clickHomeLink();
@@ -71,7 +86,7 @@ describe('GIVEN I have not authenticated', () => {
 
         describe('WHEN I click the home button', () => {
             it('THEN the login page should be shown', () => {
-                clickHomeLink();
+                cy.get('.home-button').click();
                 cy.get('.login-button').should('exist');
             });
         });
