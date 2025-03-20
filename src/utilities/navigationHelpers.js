98519e110acc35e1594e7dfcc8a46dd0015acf5b
MXC1 <mfhcor@gmail.com>
2025-03-20 16:25:39 +0000

    Only navigate if new page !== current page

--- src/utilities/navigationHelpers.js
+++ src/utilities/navigationHelpers.js
@@ -1,13 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { logMessage } from './loggingConfig';
 
 export const useNavigationHelpers = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const goTo = (path) => {
+    if (location.pathname !== path) {
       logMessage(`Navigating to ${path}`);
       navigate(path, { replace: true });
     }
+  }
   const goBack = () => navigate(-1);
 
   return { goTo, goBack };
