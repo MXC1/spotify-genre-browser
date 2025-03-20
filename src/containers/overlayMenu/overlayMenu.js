625f6f531433ec66fec03ea1e402fbe3387f765c
MXC1 <mfhcor@gmail.com>
2025-03-20 16:36:34 +0000

    Refactor OverlayMenu into App component

--- src/containers/overlayMenu/overlayMenu.js
+++ src/containers/overlayMenu/overlayMenu.js
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 import './overlayMenu.css';
-import { useNavigationHelpers } from '../../../utilities/navigationHelpers';
+import { useNavigationHelpers } from '../../utilities/navigationHelpers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
