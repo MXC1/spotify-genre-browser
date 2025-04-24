bc32e0953744856a1236ee24fe0c955684742285
MXC1 <mfhcor@gmail.com>
2025-04-24 13:31:27 +0100

    Add guard clause in loginContainer to navigate away if user is authenticated

--- src/containers/loginContainer/loginContainer.js
+++ src/containers/loginContainer/loginContainer.js
@@ -1,11 +1,22 @@
 import { useEffect } from "react";
 import { setAuthUrl } from "../../services/spotifyAuth";
+import { getCachedEntry } from "../../utilities/indexedDb";
+import { useNavigationHelpers } from "../../utilities/navigationHelpers";
 
 function LoginContainer() {
+
+  const { goTo } = useNavigationHelpers();
 
   useEffect(() => {
     window.redirectToSpotifyAuth = function () {
       setAuthUrl();
+    }
+  }, []);
+
+  useEffect(() => {
+    const token = getCachedEntry('auth', 'access_token');
+    if (token) {
+      goTo('/genre-album-map');
     }
   }, []);
 
