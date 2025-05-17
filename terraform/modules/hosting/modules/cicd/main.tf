25eba2e2617d2c1f145d0340a1610770585f12f2
MXC1 <mfhcor@gmail.com>
2025-05-17 10:35:50 +0100

    Allow CodeBuild to delete 3 objects

--- terraform/modules/hosting/modules/cicd/main.tf
+++ terraform/modules/hosting/modules/cicd/main.tf
@@ -83,6 +83,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
         Action = [
           "s3:GetObject",
           "s3:PutObject",
+          "s3:DeleteObject",
           "s3:GetObjectVersion",
           "s3:ListBucket"
         ]
