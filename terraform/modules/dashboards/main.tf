5fe72981f35eb4208cfdfd1be23e7ef0a7b601fd
MXC1 <mfhcor@gmail.com>
2025-06-02 18:49:25 +0100

    Combine SYS001 widgets

--- terraform/modules/dashboards/main.tf
+++ terraform/modules/dashboards/main.tf
@@ -19,23 +19,10 @@ resource "aws_cloudwatch_dashboard" "main" {
         width  = 12
         height = 6
         properties = {
-          query   = "SOURCE '${var.log_group_name}'\n| filter event_id = \"SYS001\"\n| stats count(*) as total_events by bin(1d) as @timestamp"
+          query   = "SOURCE '${var.log_group_name}'\n| filter event_id = \"SYS001\"\n| stats count(*) as total_events, count_distinct(session_id) as distinct_sessions by bin(1d) as @timestamp"
           region  = "eu-west-2"
-          title   = "SYS001 Events Count (Total)"
-          view    = "timeSeries"
-        }
-      },
-      {
-        type   = "log"
-        x      = 0
-        y      = 6
-        width  = 12
-        height = 6
-        properties = {
-          query = "SOURCE '${var.log_group_name}'\n| filter event_id = \"SYS001\"\n| stats count_distinct(session_id) as distinct_sessions by bin(1d) as @timestamp"
-          region = "eu-west-2"
-          title  = "SYS001 Distinct Sessions Over Time"
-          view   = "line"
+          title   = "SYS001 Events Summary"
+          view    = "table"
         }
       },
       {
