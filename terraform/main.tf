fb0b4bbc7b814d910130a0618e6762a37d8dc8ae
MXC1 <mfhcor@gmail.com>
2025-06-02 21:18:21 +0100

    WIP - remove profile from Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   region  = "eu-west-2"
-  profile = var.aws_profile != "" ? var.aws_profile : null
 }
 
 terraform {
   backend "s3" {
-    profile = "genrebrowser"
     bucket  = "genrebrowser-tf-state"
     key     = "global/s3/terraform.tfstate"
     region  = "eu-west-2"
