cffcae78f3438129a1b9f02dab93f18567b1672e
MXC1 <mfhcor@gmail.com>
2025-06-03 15:24:06 +0100

    Forcefully merged 102-do-not-redirect-to-indexhtml-for-js-files into dev

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,16 @@
-// This service worker can be used with Create React App
-// It implements a cache-first strategy for static assets and a network-first for API calls
-
-const CACHE_NAME = 'cra-cache-v1';
-const RUNTIME_CACHE = 'runtime-cache';
-
-// Assets that should be pre-cached when the service worker installs
-const PRECACHE_ASSETS = [
+const CACHE_NAME = 'genre-browser-cache-v0.26.1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/static/js/main.bundle.js', // Your main JS bundle (name might vary)
-  '/static/css/main.css',      // Your main CSS (name might vary)
   '/manifest.json',
-  '/favicon.ico'
-  // Add any other static assets your app needs
+  '/spotify-favicon.png'
 ];
 
-// Installation - Cache static assets
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(cache => cache.addAll(PRECACHE_ASSETS))
-      .then(() => self.skipWaiting()) // Force activation on all clients
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
@@ -52,19 +41,15 @@ self.addEventListener('fetch', (event) => {
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim()) // Take control of all clients
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
