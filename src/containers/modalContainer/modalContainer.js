1be4fb764e7202eac534e4483536ea6bbf09901c
MXC1 <mfhcor@gmail.com>
2025-06-11 23:54:40 +0100

    Modal UX improvements

--- src/containers/modalContainer/modalContainer.js
+++ src/containers/modalContainer/modalContainer.js
@@ -1,12 +1,31 @@
 import './modalContainer.css';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useCallback } from 'react';
 
 function ModalContainer({ isOpen, onClose, title, description, button1Text, button1Action, button2Text, button2Action, children }) {
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+        
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, [isOpen]);
+
+    const handleOverlayClick = useCallback((e) => {
+        if (e.target.className === 'modal-overlay') {
+            onClose();
+        }
+    }, [onClose]);
+
     if (!isOpen) return null;
 
     return (
-        <div className="modal-overlay">
+        <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal-content">
                 <button className="close-modal-button" onClick={onClose}>
                     <FontAwesomeIcon icon={faXmark} />
